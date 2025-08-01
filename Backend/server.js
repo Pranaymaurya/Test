eef189fad89a38faa6d6eeed6f09e187aef818f7
Pranaymaurya <139427478+Pranaymaurya@users.noreply.github.com>
2025-08-01 10:28:35 +0530

    changes made

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,8 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:4173',
-    'https://test-2vr0.onrender.com'
+    'https://test-2vr0.onrender.com',
+    'https://radiant-melomakarona-d58a1a.netlify.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
